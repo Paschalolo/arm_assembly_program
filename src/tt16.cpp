3dbe1586675c3bfa332192c8b742f646f9ad021e
Paschalolo <pastechnology1@gmail.com>
2026-09-14 09:14:56 -0900

    mesa

---
+++ src/tt16.cpp
@@ -0,0 +1,18 @@
+
+
+#include <cstdio> 
+
+extern "C" {
+	int integer_AddSubA(int a , int b , int c ) ;
+	long integer_AddSubB(long a , long b , long c);
+}
+
+integer_AddSubA
+integer_AddSubB
+int main(){
+
+	std::printf("integer_add %d\n" , integer_AddSubA(12,33,43)) ;
+	std::printf("integer_add %ld\n" , integer_AddSubB(12, 33, 43)) ;
+	return 0; 
+
+}
